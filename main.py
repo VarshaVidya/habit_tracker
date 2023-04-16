05fb193a6d92ee75b8c6bc9d4ddf796c9aaadb1c
VarshaVidya <111826112+VarshaVidya@users.noreply.github.com>
2023-04-16 23:12:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,11 @@
 import requests
 
 pixelaEndpoint = "https://pixe.la/v1/users"
-
-requests.post()
+parameters = {
+    "token":"Varsha2023",
+    "username":"varsha3",
+    "agreeTermsOfService":"yes",
+    "notMinor":"yes"
+}
+response = requests.post(url = pixelaEndpoint,json= parameters)
+print(response.text)
