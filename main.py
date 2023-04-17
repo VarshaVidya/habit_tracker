6a817d2bf9b3b72c83b2c9c32172437516744ebe
VarshaVidya <111826112+VarshaVidya@users.noreply.github.com>
2023-04-17 13:28:43 +0800

    graph

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 
 import requests
 
+USERNAME = "varsha3"
+TOKEN="Varsha2023"
+# pixela endpoint
 pixelaEndpoint = "https://pixe.la/v1/users"
 parameters = {
-    "token":"Varsha2023",
-    "username":"varsha3",
+    "token":TOKEN,
+    "username":USERNAME,
     "agreeTermsOfService":"yes",
     "notMinor":"yes"
 }
 response = requests.post(url = pixelaEndpoint,json= parameters)
 print(response.text)
+# graph endpoint
+
+graphEndpoint = f"{pixelaEndpoint}/{USERNAME}/graphs"
+graph_params={
+    "id":"graph1",
+    "name":"weight chart",
+    "unit":"Kg",
+    "type":"float",
+    "color":"shibafu"
+}
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+res = requests.post(url=graphEndpoint,json=graph_params,headers=headers)
+print(res.text)
