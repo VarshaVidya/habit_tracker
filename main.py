4fb84e4bfaf86785b0ce3e7e668e72f0f7f97b37
VarshaVidya <111826112+VarshaVidya@users.noreply.github.com>
2023-04-17 16:48:49 +0800

    automatic date generator

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 
 import requests
+from datetime import datetime
 
 USERNAME = "varsha3"
 TOKEN="Varsha2023"
+GRAPH_ID = "graph1"
 # pixela endpoint
 pixelaEndpoint = "https://pixe.la/v1/users"
 parameters = {
@@ -26,5 +28,29 @@ graph_params={
 headers = {
     "X-USER-TOKEN": TOKEN
 }
-res = requests.post(url=graphEndpoint,json=graph_params,headers=headers)
-print(res.text)
+# res = requests.post(url=graphEndpoint,json=graph_params,headers=headers)
+# print(res.text)
+today = datetime.now()
+
+pixelEndpoint= f"{pixelaEndpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+pixel_params ={
+    "date":today.strftime("%Y%m%d"),
+    "quantity":"56.5"
+    # "quantity":
+}
+pixel_header = {
+    "X-USER-TOKEN": TOKEN
+}
+resp = requests.post(url=pixelEndpoint,json=pixel_params,headers=pixel_header)
+print(resp.text)
+
+# PUT request
+putEndpoint = f"{pixelaEndpoint}/{USERNAME}/graphs/{GRAPH_ID}/{20230417}"
+putHeader = {
+    "X-USER-TOKEN": TOKEN
+}
+putBody = {
+    "quantity":"56.9"
+}
+respon = requests.put(url=putEndpoint,json= putBody,headers=putHeader)
+print(respon.text)
